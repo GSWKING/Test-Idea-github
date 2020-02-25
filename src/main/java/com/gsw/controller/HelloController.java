42d4b5246056d6d18d6af8e849ac19293f26f1b2
gsw.test <1139041834@qq.com>
2020-02-25 15:58:26 +0800

    测试开发人员提交四（开发人员提交三依然是项目经理提交）

--- src/main/java/com/gsw/controller/HelloController.java
+++ src/main/java/com/gsw/controller/HelloController.java
@@ -14,14 +14,19 @@ public class HelloController {
     public String gsw1(){
         return "Hello gsw1";
     }
-    //开发人员测试二
+    //开发人员测试二 错误发现依然是项目经理提交
     @RequestMapping("gsw2")
     public String gsw2(){
         return "Hello gsw2";
     }
-    //开发人员测试三
+    //开发人员测试三 错误发现依然是项目经理提交
     @RequestMapping("gsw3")
     public String gsw3(){
         return "Hello gsw3";
+    }
+    //开发人员测试四 错误发现依然是项目经理提交
+    @RequestMapping("gsw4")
+    public String gsw4(){
+        return "Hello gsw4";
     }
 }
