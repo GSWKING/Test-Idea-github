ce28b3f916f90e97ccf43634d83721a3c104255c
gsw.test <1139041834@qq.com>
2020-02-25 15:17:17 +0800

    测试开发人员提交二（开发人员提交一依然是项目经理提交）

--- src/main/java/com/gsw/controller/HelloController.java
+++ src/main/java/com/gsw/controller/HelloController.java
@@ -9,9 +9,14 @@ public class HelloController {
     public String info(){
         return "Hello SpringBoot Info";
     }
-    //开发人员补充
-    @RequestMapping("/gsw")
-    public String gsw(){
-        return "Hello gsw";
+    //开发人员补充 错误发现依然是项目经理提交
+    @RequestMapping("/gsw1")
+    public String gsw1(){
+        return "Hello gsw1";
+    }
+    //开发人员测试二
+    @RequestMapping("gsw2")
+    public String gsw2(){
+        return "Hello gsw2";
     }
 }
